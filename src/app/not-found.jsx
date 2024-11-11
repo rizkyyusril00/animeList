ca5cd428217cd01347cfd359ad1a014238d6cccc
rizkyyusril <rizkyyusril74@gmail.com>
2024-11-11 16:13:24 +0700

    fix bug

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,17 +1,19 @@
-import Link from "next/link";
+'use client'
 
+import { useRouter } from "next/navigation";
 export default function NotFound() {
+    const router = useRouter();
     return (
         <>
             <div className="flex flex-col gap-2 items-center justify-center h-screen">
                 <span className="text-[40px] text-acent">404</span>
                 <span className="text-[40px] text-acent">Page Not Found</span>
-                <Link
-                href="/"
-                className="p-2 w-[140px] max-w-[140px] rounded-md text-center bg-acent text-secondary hover:bg-secondary hover:text-acent transition-all duration-500 ease-in-out"
+                <button
+                onClick={() => router.back()}
+                className="p-2 w-[140px] max-w-[140px] rounded-md text-center bg-acent text-secondary hover:bg-secondary hover:text-acent transition-all duration-300 ease-in-out"
                 >
                     <span>Back</span>
-                </Link>
+                </button>
             </div>
         </>
     );
